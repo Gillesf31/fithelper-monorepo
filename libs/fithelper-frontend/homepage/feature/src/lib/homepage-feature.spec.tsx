68414910f4319f8436552859142c09a77a1ce949
Gilles Ferrand <gilles.ferrand31@gmail.com>
2025-08-16 21:08:35 -0400

    Fix tasks to add commit hooks

--- libs/fithelper-frontend/homepage/feature/src/lib/homepage-feature.spec.tsx
+++ libs/fithelper-frontend/homepage/feature/src/lib/homepage-feature.spec.tsx
@@ -1,10 +1,15 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import HomepageFeature from './homepage-feature';
 
 describe('HomepageFeature', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<HomepageFeature />);
+    const { baseElement } = render(
+      <MemoryRouter>
+        <HomepageFeature />
+      </MemoryRouter>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
